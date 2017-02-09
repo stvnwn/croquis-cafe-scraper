5a9ea0716fd0d19f10ee3de6a7032ed77e932b98
Steven Huynh <stevenhuynh@ste.vin>
2017-02-09 17:33:59 -0500

    Handle HTTP 302 responses from www.onairvideo.com
    Sometimes www.onairvideo.com responds with the status code 302.
    Subsequent requests to the same www.onairvideo.com resource will result
    in HTTP 200 responses.

--- scrape.js
+++ scrape.js
@@ -5,6 +5,31 @@ var path = require('path');
 
 var archivePath = process.argv[2];
 
+function getHTML(hostname, path) {
+  return new Promise(function (resolve, reject) {
+    http.get({
+      hostname: hostname,
+      path: path
+    }, function (res) {
+      if (res.statusCode == 302) {
+        getHTML(hostname, path).then(function (nestedHTML) {
+          resolve(nestedHTML);
+        });
+      }
+      else {
+        var HTML = '';
+
+        res.on('data', function (data) {
+          HTML += data;
+        });
+        res.on('end', function () {
+          resolve(HTML);
+        });
+      }
+    });
+  });
+}
+
 // check for command-line argument
 if (archivePath === undefined) {
   console.log('Missing path to directory');
@@ -22,64 +47,42 @@ if (fs.existsSync(archivePath)) {
 // make directory
 fs.mkdirSync(archivePath);
 
-// scrape links to models, then scrape models synchronously
-http.get({
-  hostname: 'www.onairvideo.com',
-  path: '/croquis-cafe-photos.html'
-}, function (res) {
-  var archiveHTML = '';
+// scrape links to models, then scrape models
+getHTML('www.onairvideo.com', '/croquis-cafe-photos.html').then(function (archiveHTML) {
+  var models = archiveHTML.match(/cc-photo-archive.{2,}?\.html/g);
 
-  res.on('data', function (data) {
-    archiveHTML += data;
-  });
-  res.on('end', function () {
-    // extract links to models from directory
-    var models = archiveHTML.match(/cc-photo-archive.{2,}?\.html/g);
+  // scrape links to a model's photos, then download photos
+  function scrapeModel(modelIndex) {
+    getHTML('www.onairvideo.com', '/' + models[modelIndex]).then(function (modelHTML) {
+      var photos = modelHTML.match(/"src":"\/\/nebula\.wsimg\.com\/\w{32}\?AccessKeyId=05ECB8D9DFC0F8678544&disposition=0&alloworigin=1"/g);
+      var modelDir = models[modelIndex].substring(17, models[modelIndex].length - 5);
+ 
+      // download photo to a model's directory
+      // once done, repeat with remaining photos and other models
+      function downloadPhoto(photoIndex) {
+        http.get({
+          hostname: 'nebula.wsimg.com',
+          path: photos[photoIndex].substring(25, 91)
+        }, function (res) {
+          var photo = fs.createWriteStream(path.join(archivePath, modelDir, photoIndex + '.jpg'));
+          res.pipe(photo);
 
-    // scrape links to a model's photos, then download photos synchronously
-    function scrapeModel(modelIndex) {
-      http.get({
-        hostname: 'www.onairvideo.com',
-        path: '/' + models[modelIndex]
-      }, function (res) {
-        var modelHTML = '';
-
-        res.on('data', function (data) {
-          modelHTML += data;
-        });
-        res.on('end', function () {
-          // extract links to photos from a model's page
-          var photos = modelHTML.match(/"src":"\/\/nebula\.wsimg\.com\/\w{32}\?AccessKeyId=05ECB8D9DFC0F8678544&disposition=0&alloworigin=1"/g);
-          var modelDir = models[modelIndex].substring(17, models[modelIndex].length - 5);
-
-          // download photo to a model's directory
-          // once done, repeat with remaining photos and other models
-          function downloadPhoto(photoIndex) {
-            http.get({
-              hostname: 'nebula.wsimg.com',
-              path: photos[photoIndex].substring(25, 91)
-            }, function (res) {
-              var photo = fs.createWriteStream(path.join(archivePath, modelDir, photoIndex + '.jpg'));
-              res.pipe(photo);
-
-              res.on('end', function () {
-                if (photoIndex + 1 < photos.length) {
-                  downloadPhoto(photoIndex + 1);
-                } else if (modelIndex + 1 < models.length) {
-                  scrapeModel(modelIndex + 1);
-                }
-              });
-            });
-          }
-
-          // make directories within the given directory that correspond to the model's pages
-          fs.mkdir(path.join(archivePath, modelDir), function () {
-            downloadPhoto(0);
+          res.on('end', function () {
+            if (photoIndex + 1 < photos.length) {
+              downloadPhoto(photoIndex + 1);
+            } else if (modelIndex + 1 < models.length) {
+              scrapeModel(modelIndex + 1);
+            }
           });
         });
-      });
-    }
+      }
 
-    scrapeModel(0);
-  });
+      // make directories within the given directory that correspond to the model's pages
+      fs.mkdir(path.join(archivePath, modelDir), function () {
+        downloadPhoto(0);
+      });
+    });
+  }
+
+  scrapeModel(0);
 });
